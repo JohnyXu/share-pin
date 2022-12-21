0ac0bf6ed98d086ea2329fcfe0a56b2937366925
Johny Xu <ahstuxq@gmail.com>
2022-12-21 17:39:02 +1100

    add video mp4 as background

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import logo from '../assets/logowhite.png';
+import shareVideo from '../assets/share.mp4';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="realative w-full h-full">
+        <video
+          src={shareVideo}
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
