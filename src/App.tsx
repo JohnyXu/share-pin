fb5fba92bc89840716e7fe7c52d70bc8146b80c1
Johny Xu <ahstuxq@gmail.com>
2022-12-21 17:26:22 +1100

    add router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { Login, Home } from './components';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
