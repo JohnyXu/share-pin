ddacf40105848c844235c9e09aa9d53f33e5239d
Johny Xu <ahstuxq@gmail.com>
2022-12-23 11:54:12 +1100

    add sanity backend schema and config

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5wnma3lj',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import user from './user'
+import comment from './comment'
+import save from './save'
+import postedBy from './postedBy'
+import pin from './pin'
+
+export const schemaTypes = [user, save, comment, postedBy, pin]
