ce2568b6c6aa34e4d6a30cb62e71c9400acd7264
Johny Xu <ahstuxq@gmail.com>
2022-12-27 15:49:03 +1100

    add search bar and remove react logo

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import MasonryLayout from './MasonryLayout';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import Spinner from './Spinner';
+import { IPin } from '../types';
 
 interface SearchProps {
   searchTerm: string;
   setSearchTerm: (value: string) => void;
 }
 
-function Search(props: SearchProps) {
-  return <div>Search</div>;
+function Search({ searchTerm }: SearchProps) {
+  const [pins, setPins] = useState<IPin[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== '') {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching pins" />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl ">No Pins Found!</div>
+      )}
+    </div>
+  );
 }
 
 export default Search;
